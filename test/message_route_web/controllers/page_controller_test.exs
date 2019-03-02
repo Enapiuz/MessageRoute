564d355b42703e4a7ea81109ffb226f6d468deec
Vadim Shashin <enapiuz@gmail.com>
2019-03-02 17:51:41 +0300

    fix tests

--- test/message_route_web/controllers/page_controller_test.exs
+++ test/message_route_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule MessageRouteWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Message Route"
   end
 end
