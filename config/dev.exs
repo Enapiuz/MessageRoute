ec9af2ee56b89dc34f33a9ef92099b2dc6cbd7c8
Vadim Shashin <enapiuz@gmail.com>
2019-03-03 17:19:25 +0300

    slack token from settings

--- config/dev.exs
+++ config/dev.exs
@@ -73,5 +73,3 @@ config :message_route, MessageRoute.Repo,
   database: "message_route_dev",
   hostname: "localhost",
   pool_size: 10
-
-import_config "dev.secret.exs"
--- lib/message_route/slack_channel/slack_channel.ex
+++ lib/message_route/slack_channel/slack_channel.ex
@@ -1,6 +1,7 @@
 defmodule MessageRoute.SlackChannel do
   require Logger
   alias MessageRoute.Exchange.Command
+  alias MessageRoute.Settings
 
   @spec send(MessageRoute.Exchange.Command.t()) :: MessageRoute.Exchange.Command.t()
   def send(%Command{user: user} = command) do
@@ -16,6 +17,10 @@ defmodule MessageRoute.SlackChannel do
         Logger.error("Error calling slack api", error)
         {:error, command}
 
+    {:error, :token_not_set} ->
+        Logger.error("Slack API token not set")
+        {:error, command}
+
       :error ->
         Logger.error("Invalid data in :members or :email")
         {:error, command}
@@ -27,7 +32,13 @@ defmodule MessageRoute.SlackChannel do
   end
 
   defp get_slack_user_list do
-    Slack.Web.Users.list()
+    case Settings.get("slack_token") do
+      {:ok, slack_token} ->
+        Slack.Web.Users.list(%{token: slack_token})
+
+      {:error} ->
+        {:error, :token_not_set}
+    end
   end
 
   defp get_target_member(members, email) do
@@ -49,8 +60,13 @@ defmodule MessageRoute.SlackChannel do
   defp send_by_id(member, %{body: body} = command) do
     case member do
       %{"id" => id} ->
-        Slack.Web.Chat.post_message(id, body)
-        {:ok, command}
+        case Settings.get("slack_token") do
+          {:ok, slack_token} ->
+            Slack.Web.Chat.post_message(id, body, %{token: slack_token})
+            {:ok, command}
+          {:error} ->
+            {:error, :token_not_set}
+        end
 
       error ->
         Logger.error("Error calling slack post_message: #{inspect(error)}")
